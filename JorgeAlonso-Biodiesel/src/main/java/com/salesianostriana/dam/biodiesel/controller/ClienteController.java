bd3eb9b34f086a4dc1f82ac13c92ae75a43717a3
Jorge Alonso <jorgealonso7@outlook.es>
2020-05-09 13:11:43 +0200

    test form

--- JorgeAlonso-Biodiesel/src/main/java/com/salesianostriana/dam/biodiesel/controller/ClienteController.java
+++ JorgeAlonso-Biodiesel/src/main/java/com/salesianostriana/dam/biodiesel/controller/ClienteController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.salesianostriana.dam.biodiesel.modelo.Cliente;
 import com.salesianostriana.dam.biodiesel.servicio.ClienteServicio;
+import com.salesianostriana.dam.biodiesel.servicio.PaisServicio;
 
 @Controller
 public class ClienteController {
@@ -15,12 +18,26 @@ public class ClienteController {
 	@Autowired
 	private ClienteServicio servicio;
 	
+	@Autowired
+	private PaisServicio servicioPais;
+	
 	
 	@GetMapping ("/nuevo")
 	public String registro(Model model) {
-		
+		model.addAttribute("clienteFrom",new Cliente());
+		model.addAttribute("listaPais", servicioPais.findAll());
 		return "/cliente/Nuevo";
 	}
+	
+	
+	
+	@PostMapping ("/nuevo/submit")
+	public String submitRegistro(@ModelAttribute("clienteFrom") Cliente nuevoCliente) {
+		servicio.save(nuevoCliente);
+		return "redirect:/cliente";
+	}
+	
+	
 	
 	@GetMapping ("/login")
 	public String login(Model model) {
